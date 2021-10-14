2373d03ed2977cb4f88d999d9c48a952423fc15b
IrgaLansh <irgala@tut.by>
2021-10-14 23:50:05 +0300

    basetask refactoring

--- Project04/src/BaseTask/TrainMethodsIf.java
+++ Project04/src/BaseTask/TrainMethodsIf.java
@@ -6,37 +6,40 @@ public class TrainMethodsIf {
     private String characterCombo;
 
     public int returnNewInt(int intNumber) {
-        return intNumber < 8 ? intNumber *= 7 : intNumber /= 4;
+        if (intNumber < 8) {intNumber *= 7;} else{ intNumber /= 4;}
+        return intNumber;
 //        System.out.println("method returnNewInt returns " + intNumber);
     }
 
-    public int returnNewLong(long longNumber) {
+    public long returnNewLong(long longNumber) {
         if (longNumber > 300) {
             longNumber -= 300;
         } else {
             longNumber += 20;
         }
-        System.out.println("method returnNewLong returns " + longNumber);
+        return longNumber;
+//        System.out.println("method returnNewLong returns " + longNumber);
     }
 
-    public int returnNewChar(char character) {
+    public String returnNewChar(char character) {
         if (character == 'g') {
             characterCombo = character + "o";
         } else {
             characterCombo = "o" + character;
         }
         System.out.println("method returnNewChar returns " + characterCombo);
+        return characterCombo;
     }
 
-    public int returnNewFloat(float floatNumber) {
+    public float returnNewFloat(float floatNumber) {
         if (floatNumber != 0.67) {
             floatNumber *= 2;
         }
         return floatNumber;
-        System.out.println("method returnNewFloat returns " + floatNumber);
+        //System.out.println("method returnNewFloat returns " + floatNumber);
     }
 
-    public int returnNewDouble(double doubleNumber) {
+    public double returnNewDouble(double doubleNumber) {
         if (doubleNumber > 30 & doubleNumber < 80) {
             doubleNumber += 87;
         } else {
@@ -49,9 +52,11 @@ public class TrainMethodsIf {
             }
         }
         System.out.println("method returnNewDouble returns " + doubleNumber);
+        return doubleNumber;
     }
 
-    public int returnNewBoolean(boolean booleanValue) {
+    public boolean returnNewBoolean(boolean booleanValue) {
         System.out.println("I receive " + booleanValue);
+        return booleanValue;
     }
 }
