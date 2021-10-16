6d1227b5fbbe7702a73e7ab7f8d80bbc7865ee21
IrgaLansh <irgala@tut.by>
2021-10-16 15:51:17 +0300

    day05 classwork

---
+++ Project04/src/Main/Java/tasks/classwork/day05/DemoMath.java
@@ -0,0 +1,29 @@
+package tasks.classwork.day05;
+
+import java.util.Random;
+
+import static java.lang.String.valueOf;
+
+public class DemoMath {
+    public static void main(String[] args) {
+
+        float xx = 0.3f;
+        float yy = 0.4f;
+        //String.
+        int gg = Integer.parseInt("222");
+        int hh = Integer.parseInt("bhjdbhbj");
+        System.out.println(gg);
+
+        int n = 5;
+        String s = "";
+        for (int i = 0; i < n; i++) {
+            s += "";
+
+        }
+
+
+        //System.out.println(xx + yy);
+
+
+    }
+}
