0c16fd15591a546c83f21f59c61feb56b0b94455
IrgaLansh <irgala@tut.by>
2021-10-29 23:12:18 +0300

    homework day8-1

---
+++ Project04/src/Main/Java/tasks/homework/day8hw/objectClasses/Sand.java
@@ -0,0 +1,61 @@
+package tasks.homework.day8hw.objectClasses;
+
+/*Создать пустой список песка (класса Sand) sandbox
+Заполнить 4 обьектами песка с весом (2, 4, 2, 7) и именами (Речной, Речной, Карьерный, Речной) соответственно
+Проитерировать список через for-each и отпечатать вес в консоль через пробел
+Проитерировать список через for-each и отпечатать имена в консоль через пробел
+Создать пустую карту, где ключем является целое число, а значением обьект песка
+Заполнить карту значениями из предыдущего списка, используя в качестве ключа любое уникальное число
+Проитерировать карту и отпечатать ключи в консоль
+Проитерировать карту и отпечатать значения в консоль
+Проитерировать карту и отпечатать пары ключ-значение в консоль*/
+
+import java.util.Objects;
+
+public class Sand {
+
+    private int weight;
+    private String name;
+
+    public Sand(int weight, String name) {
+        this.weight = weight;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Sand{" +
+                "weight=" + weight +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Sand sand = (Sand) o;
+        return weight == sand.weight && Objects.equals(name, sand.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(weight, name);
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
